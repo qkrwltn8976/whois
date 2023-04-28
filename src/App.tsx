8a4c0b6e0c88de72c79fcdfa11535f3e2727f4e7
earthpfasoo <qkrwltn8976@naver.com>
2023-04-28 10:31:55 +0900

    feat: SPA 초기 로딩 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Search from "@/search/container/Search";
 import User from "@/user/container/User";
 import "antd/dist/reset.css";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
+  useEffect(() => {
+    const bodyEl = document.getElementsByTagName("body")[0];
+    const loadingEl = document.getElementById("init-loading");
+    if (loadingEl) {
+      bodyEl.removeChild(loadingEl);
+    }
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Search />} />
